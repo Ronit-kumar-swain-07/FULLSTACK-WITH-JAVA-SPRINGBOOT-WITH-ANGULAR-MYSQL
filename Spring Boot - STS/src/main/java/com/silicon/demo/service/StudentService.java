d430af01e977f0321bf1a0bb651c1437295c013e
Ronit-kumar-swain-07 <kumarswainronit@gmail.com>
2026-07-01 23:17:22 +0530

    Commit

--- Spring Boot - STS/src/main/java/com/silicon/demo/service/StudentService.java
+++ Spring Boot - STS/src/main/java/com/silicon/demo/service/StudentService.java
@@ -1,14 +1,19 @@
 package com.silicon.demo.service;
 
-import com.silicon.demo.model.StudentDetails;
 import java.util.List;
+
+import com.silicon.demo.model.StudentDetails;
 
 public interface StudentService {
 
 	
 	List<StudentDetails> getStudentDetails();
 	
+	
+//	Get a Student Detail by ID
 	StudentDetails getStudentByID(int id);
 	
+	
+	StudentDetails saveStudentDetails(StudentDetails student);
 	
 }
