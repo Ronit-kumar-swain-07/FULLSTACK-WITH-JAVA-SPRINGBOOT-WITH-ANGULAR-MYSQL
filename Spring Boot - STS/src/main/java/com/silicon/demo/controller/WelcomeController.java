92e784710163680c805082e9d666e2381eb27ea3
Ronit-kumar-swain-07 <kumarswainronit@gmail.com>
2026-07-03 00:47:28 +0530

    Commit

--- Spring Boot - STS/src/main/java/com/silicon/demo/controller/WelcomeController.java
+++ Spring Boot - STS/src/main/java/com/silicon/demo/controller/WelcomeController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,8 @@ import com.silicon.demo.service.StudentService;
 
 import io.swagger.v3.oas.annotations.Operation;
 
+
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/home")
 public class WelcomeController {
